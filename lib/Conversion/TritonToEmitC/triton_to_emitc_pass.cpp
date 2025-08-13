292e34b56f7612cca535a580fff1f5cc31b4783d
maoyanpeng <545098911@qq.com>
2025-08-13 14:10:42 +0800

    support call, func, return

--- lib/Conversion/TritonToEmitC/triton_to_emitc_pass.cpp
+++ lib/Conversion/TritonToEmitC/triton_to_emitc_pass.cpp
@@ -31,15 +31,146 @@
 
 namespace mlir::triton {
 
+static void addNamedAttrs(Operation *op, DictionaryAttr dictAttrs) {
+  for (const NamedAttribute attr : dictAttrs.getValue())
+    if (!op->hasAttr(attr.getName()))
+      op->setAttr(attr.getName(), attr.getValue());
+}
+
+class TritonToEmitCTypeConverter : public mlir::TypeConverter {
+public:
+  TritonToEmitCTypeConverter() {
+    // 添加默认转换：保留标准类型（如整数、浮点数）
+    addConversion([](Type type) -> std::optional<Type> {
+      return type;
+    });
+
+    // 转换 Triton 指针类型 -> EmitC 指针类型
+    addConversion([](triton::PointerType type) -> Type {
+      return emitc::PointerType::get(type.getPointeeType());
+    });
+
+    // 转换 Triton Tensor 类型（可能需要展平或特殊处理）
+    addConversion([](RankedTensorType tensorType) -> Type {
+      return emitc::ArrayType::get(tensorType.getShape(), tensorType.getElementType());
+    });
+
+    addSourceMaterialization([&](OpBuilder &builder, Type resultType,
+                                ValueRange inputs,
+                                Location loc) -> std::optional<Value> {
+      if (inputs.size() != 1) return std::nullopt;
+      return builder.create<UnrealizedConversionCastOp>(loc, resultType, inputs).getResult(0);
+    });
+    addTargetMaterialization([&](OpBuilder &builder, Type resultType,
+                                ValueRange inputs,
+                                Location loc) -> std::optional<Value> {
+      if (inputs.size() != 1) return std::nullopt;
+      return builder.create<UnrealizedConversionCastOp>(loc, resultType, inputs).getResult(0);
+    });
+  }
+};
+
+class CallOpConversion final : public OpConversionPattern<triton::CallOp> {
+public:
+  using OpConversionPattern<triton::CallOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(triton::CallOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    // Multiple results func was not converted to `emitc.func`.
+    if (op.getNumResults() > 1) {
+      return rewriter.notifyMatchFailure(op, "only functions with zero or one result can be converted");
+    }
+
+    auto newOp = rewriter.replaceOpWithNewOp<emitc::CallOp>(
+        op, op.getCallee(), op.getResultTypes(), adaptor.getOperands());
+    addNamedAttrs(newOp, adaptor.getAttributes());
+    return success();
+  }
+};
+
+class FuncOpConversion final : public OpConversionPattern<triton::FuncOp> {
+public:
+  using OpConversionPattern<triton::FuncOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(triton::FuncOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    // op->getParentOfType<ModuleOp>().dump();
+    // 1. 转换函数签名类型
+    auto typeConverter = getTypeConverter();
+    mlir::TypeConverter::SignatureConversion signatureConverter(
+        op.getFunctionType().getNumInputs());
+    for (const auto &arg : llvm::enumerate(op.getArguments())) {
+      mlir::Type convertedType = typeConverter->convertType(arg.value().getType());
+      signatureConverter.addInputs(arg.index(), convertedType);
+    }
+    // 转换返回类型
+    mlir::SmallVector<mlir::Type> convertedResultTypes;
+    if (failed(typeConverter->convertTypes(op.getResultTypes(), convertedResultTypes))) {
+      return failure();
+    }
+
+    // 2. 创建新的 emitc.func 操作
+    auto newFuncType = mlir::FunctionType::get(
+        op.getContext(),
+        signatureConverter.getConvertedTypes(),
+        convertedResultTypes
+    );
+    auto newFunc = rewriter.create<emitc::FuncOp>(
+        op.getLoc(),
+        op.getName(),
+        newFuncType
+    );
+    for (const auto &namedAttr : op->getAttrs()) {
+      if (namedAttr.getName() != op.getFunctionTypeAttrName() &&
+          namedAttr.getName() != SymbolTable::getSymbolAttrName())
+        newFunc->setAttr(namedAttr.getName(), namedAttr.getValue());
+    }
+
+    // 3. 迁移函数体并重写参数类型
+    rewriter.inlineRegionBefore(op.getBody(), newFunc.getBody(), newFunc.end());
+    if (failed(rewriter.convertRegionTypes(&newFunc.getBody(), *typeConverter, &signatureConverter))) {
+      return failure();
+    }
+
+    // 4. 删除原 Triton func.func
+    rewriter.eraseOp(op);
+
+    return success();
+  }
+};
+
+class ReturnOpConversion final : public OpConversionPattern<triton::ReturnOp> {
+public:
+  using OpConversionPattern<triton::ReturnOp>::OpConversionPattern;
+
+  LogicalResult
+  matchAndRewrite(triton::ReturnOp op, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    if (op.getNumOperands() > 1) {
+      return rewriter.notifyMatchFailure(op, "only zero or one operand is supported");
+    }
+
+    rewriter.replaceOpWithNewOp<emitc::ReturnOp>(
+        op,
+        op.getNumOperands() ? adaptor.getOperands()[0] : nullptr);
+    return success();
+  }
+};
+
 template <typename TritonOp, typename EmitCOp>
-class TritonOpConversion final : public OpConversionPattern<TritonOp> {
+class GenericOpPattern final : public OpConversionPattern<TritonOp> {
 public:
   using OpConversionPattern<TritonOp>::OpConversionPattern;
 
-  LogicalResult matchAndRewrite(TritonOp arithOp, typename TritonOp::Adaptor adaptor,
+  LogicalResult matchAndRewrite(TritonOp op, typename TritonOp::Adaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
-    rewriter.template replaceOpWithNewOp<EmitCOp>(arithOp, arithOp.getType(),
-                                                  adaptor.getOperands());
+    SmallVector<Type> retTypes;
+    if (failed(this->getTypeConverter()->convertTypes(op->getResultTypes(), retTypes))) {
+      return failure();
+    }          
+    rewriter.template replaceOpWithNewOp<EmitCOp>(op, retTypes, adaptor.getOperands(), op->getAttrs());
 
     return success();
   }
@@ -47,10 +178,11 @@ public:
 
 void populate_triton_to_emitc_patterns(TypeConverter &typeConverter, RewritePatternSet &patterns) {
   MLIRContext *ctx = patterns.getContext();
-
   // clang-format off
   patterns.add<
-    // TritonOpConversion<triton::FuncOp, emitc::FuncOp>,
+    CallOpConversion,
+    FuncOpConversion,
+    ReturnOpConversion,
     TritonOpConversion<triton::GetProgramIdOp, emitc::GetProgramIdOp>
   >(typeConverter, ctx, /*benefit=*/1);
   // clang-format on
@@ -65,12 +197,15 @@ class ConvertTritonToEmitC : public ConvertTritonToEmitCBase<ConvertTritonToEmit
     target.addLegalDialect<emitc::EmitCDialect>();
     target.addLegalDialect<emitc_ext::EmitCExtDialect>();
     // target.addIllegalDialect<triton::TritonDialect>();
-    target.addIllegalOp<triton::GetProgramIdOp>();
+    target.addIllegalOp<
+      triton::CallOp,
+      triton::FuncOp,
+      triton::ReturnOp,
+      triton::GetProgramIdOp
+    >();
 
     RewritePatternSet patterns(&getContext());
-
-    TypeConverter typeConverter;
-    typeConverter.addConversion([](Type type) { return type; });
+    TritonToEmitCTypeConverter typeConverter;
 
     populate_triton_to_emitc_patterns(typeConverter, patterns);
 
